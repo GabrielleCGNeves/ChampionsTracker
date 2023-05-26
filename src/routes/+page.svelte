0922404c3a97d774406f518c3ada7752d15fb0bc
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-05-26 16:49:47 -0300

    💄 button background-color
    Co-authored-by: Gabrielle Carvalho <GabrielleCGNeves@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,19 @@
         Titulo
     </h1>
 
-    <Butao />
+    <Butao ref="seeMore" texto={"VER MAIS"} />
+
 </div>
 
 <p>
     Lorem ipsum dolor sit amet nobis duo. Amet duis et est diam dolores ipsum accusam tempor dolor tempor iriure at. Vulputate soluta dolore erat. Vero erat sanctus laoreet. Kasd nonumy lorem diam wisi est. Rebum takimata amet dolores dolor euismod commodo in ipsum gubergren esse diam stet at nulla erat. Sea erat tempor lorem ut amet et diam vero gubergren quod sit stet at invidunt. Sea et esse et justo ut vero soluta lorem at stet in ea. Velit ipsum gubergren dolore. Tempor duo vero sanctus facilisis. Sed diam dolore lorem takimata nisl ut vel sadipscing sanctus ipsum sed consetetur invidunt et suscipit lorem. Luptatum vel luptatum at. Et sed sed nonummy sadipscing lorem stet et sit est sit sed takimata sed vel consequat dolor diam. Eos et erat kasd ea wisi sadipscing consequat eu aliquyam ipsum ipsum takimata esse aliquyam imperdiet augue et eos. Nonumy ipsum et ipsum tempor duis clita. Rebum lorem et consetetur lobortis nulla dolor at. Stet at ea lorem laoreet magna ipsum amet nulla et odio.
 </p>
+
+    <style>
+        :global([ref=seeMore]) {
+            background-color: var(--tertiary-color);
+        }
+        :global([ref=seeMore]):hover {
+            background-color: var(--secondary-color);
+        }
+    </style>
