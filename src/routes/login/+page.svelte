e7fbf3584fb4ffbc3d6815b7e8c964bd0770ac86
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-05-26 16:50:15 -0300

    ✨ login page
    Co-authored-by: Gabrielle Carvalho <GabrielleCGNeves@users.noreply.github.com>

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Butao from "$lib/components/Butao.svelte";
+</script>
+
+<p class="title">Logue no Champions Tracker</p>
+<p class="details">Logar com</p>
+
+<div class="grid-container">
+    <div class="grid-item">
+        <button class="discord">Discord</button>
+        <button class="twitter">Twitter</button>
+        <button class="google">Google</button>
+    </div>
+</div>
+
+<form action="POST" class="forms">
+    <input type="text" class="input" name="username" placeholder="Nome de Usuário ou Email">
+    <br>
+    <input type="password" class="input" name="pass" placeholder="Senha">
+</form>
+
+<div class="div">
+    <input type="checkbox" name="rememberMe">
+    <label for="" class="details">Lembrar minha senha</label>
+</div>
+
+<Butao ref="confirmar" texto={"LOGAR"} />
+
+<style>
+    :global([ref=confirmar]) {
+        background-color: var(--secondary-color);
+    }
+    :global([ref=confirmar]):hover {
+        background-color: var(--forms-color);
+    }
+</style>
