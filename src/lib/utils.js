66a28a25edb85760cd9edf3be1b25e2165cf8b9f
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-09-11 10:38:58 -0300

    🔥 removed pocketbase

--- src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-/**
- * Serialize a non-plain old JavaScript object (POJO) using structured clone algorithm.
- *
- * @param {any} obj - The object to be serialized.
- * @return {any} Returns the serialized object.
- */
-export const serializeNonPOJOs = (obj) => {
-    return structuredClone(obj)
-};
