0ba3a3f3ca1220d71a40098e214b5881db0c0ec1
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-09-11 10:39:49 -0300

    🎨 centered login page
    Co-authored-by: Gabrielle Carvalho <GabrielleCGNeves@users.noreply.github.com>

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,29 +2,31 @@
     import Butao from "$lib/components/Butao.svelte";
 </script>
 
-<p class="page-title">Logue no Champions Tracker</p>
-<p class="details">Logar com</p>
-
-<div class="grid-container">
-    <div class="grid-item">
-        <button class="discord">Discord</button>
-        <button class="twitter">Twitter</button>
-        <button class="google">Google</button>
+<main class="main-content">
+    <p class="page-title">Logue no Champions Tracker</p>
+    <p class="details">Logar com</p>
+    
+    <div class="grid-container">
+        <div class="grid-item">
+            <button class="discord">Discord</button>
+            <button class="twitter">Twitter</button>
+            <button class="google">Google</button>
+        </div>
     </div>
-</div>
-
-<form action="POST" class="forms">
-    <input type="text" class="input" name="username" placeholder="Nome de Usuário ou Email">
-    <br>
-    <input type="password" class="input" name="pass" placeholder="Senha">
-</form>
-
-<div class="div">
-    <input type="checkbox" name="rememberMe">
-    <label for="" class="details">Lembrar minha senha</label>
-</div>
-
-<Butao ref="confirmar" texto={"LOGAR"} />
+    
+    <form action="POST" class="forms">
+        <input type="text" class="input" name="username" placeholder="Nome de Usuário ou Email">
+        <br>
+        <input type="password" class="input" name="pass" placeholder="Senha">
+    </form>
+    
+    <div class="div">
+        <input type="checkbox" name="rememberMe">
+        <label for="" class="details">Lembrar minha senha</label>
+    </div>
+    
+    <Butao ref="confirmar" texto={"LOGAR"} />
+</main>
 
 <style>
     :global([ref=confirmar]) {
