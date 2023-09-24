09942bb2f02f3d9cb4115e3bf58830403ce12207
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-09-23 22:38:22 -0300

    🛂 restricted user team deletion

--- src/routes/feed/[teamid]/+page.server.js
+++ src/routes/feed/[teamid]/+page.server.js
@@ -1,18 +1,38 @@
 import { prisma } from "$lib/server/prisma";
 import { error } from "@sveltejs/kit"
 
-export const load = async ({ params }) => {
+export const load = async ({ locals, params }) => {
+    // Check if the user is authenticated
+    if (!locals.user) {
+        // If not, throw a redirect exception to the home page
+        throw redirect(302, '/')
+    }
+
+    // Define an async function to get the team information
     const getTeam = async () => {
+        // Retrieve the team from the database based on the team ID
         const team = await prisma.tb_equipe.findUnique({
             where: {
                 equ_idequipe: Number(params.teamid)
             }
         })
+
+        // If the team is not found, throw an exception with a 404 status code
         if (!team) {
-            throw error(404, 'Equipe não encontrada')
+            throw error(404, 'Equipe não encontrada')
         }
+
+        // Check if the user's ID is not equal to the team's user ID
+        if (locals.user.id != team.usu_idusuario) {
+            // If it's not equal, throw a 403 error with the message 'Acesso não autorizado'
+            throw error(403, 'Acesso não autorizado');
+        }
+
+        // Return the team information
         return team
     }
+
+    // Return an object with the team information
     return {
         equipe: getTeam(),
     }
@@ -24,8 +44,8 @@ export const actions = {
         const name = data.get("teamName")
         const number = parseInt(data.get("teamNumber"))
         const description = data.get("teamDescription")
-        const image = data.get("image") 
- 
+        const image = data.get("image")
+
 
         try {
             await prisma.tb_equipe.update({
