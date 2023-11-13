0dec737f355c49097ad2f601af29cc6e0c886024
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-11-12 21:30:17 -0300

    💄 better navbar logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,28 @@
 <script>
-import '../style.css'
-import Navbar from "$lib/components/Navbar.svelte";
-import { page } from '$app/stores'
+    import "../style.css";
+    import Navbar from "$lib/components/Navbar.svelte";
+    import { page } from "$app/stores";
+    import { derived } from "svelte/store";
+
+    // This derived store computes whether the navbar should be shown
+    const showNavbar = derived(page, ($page) => {
+        // Check if the current path is "/" or starts with "/tournament"
+        return !(
+            $page.url.pathname === "/" ||
+            $page.url.pathname.startsWith("/tournament-view")
+        );
+    });
 </script>
 
 <div>
-    {#if  $page.url.pathname === '/'}
-    
-        <main>
-            <slot />
-        </main>
-
-    {:else}
-
+    {#if $showNavbar}
         <Navbar />
-
-        <main>
-            <slot />
-        </main>
-
     {/if}
+
+    <main>
+        <slot />
+    </main>
 </div>
 
 <style>
-
 </style>
