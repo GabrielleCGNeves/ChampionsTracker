62e73b92e23f27c53c28db5f4cfa2394978e1d72
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-11-13 23:36:16 -0300

    ✨ team request logic

--- src/routes/tournament-view/[tournamentId]/+page.server.js
+++ src/routes/tournament-view/[tournamentId]/+page.server.js
@@ -1,6 +1,24 @@
+import { redirect, fail, error } from '@sveltejs/kit';
+import { prisma } from '$lib/server/prisma'
+
 /** @type {import('./$types').Actions} */
 export const actions = {
-    requestEntry: async ({ request, params, locals }) => {
-        
+requestEntry: async ({ request, params, locals }) => {
+    const { equipePedido } = Object.fromEntries(await request.formData());
+    
+    if (!equipePedido) {
+        return fail(400, { message: "Selecione uma equipe" });
     }
+
+    try {
+        await prisma.PedidoCampeonato.create({
+            data: {
+                campeonatoId: Number(params.tournamentId),
+                equipeId: Number(equipePedido)
+            }
+        });
+    } catch (error) {
+        return fail(500, { message: "Não foi possível pedir a participação" });
+    }
+}
 };
