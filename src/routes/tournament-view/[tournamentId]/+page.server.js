be103fedfcd36498e8d8279a2e550e409728cc26
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-11-20 21:42:56 -0300

    🦺 added new validations

--- src/routes/tournament-view/[tournamentId]/+page.server.js
+++ src/routes/tournament-view/[tournamentId]/+page.server.js
@@ -3,22 +3,48 @@ import { prisma } from '$lib/server/prisma'
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-requestEntry: async ({ request, params, locals }) => {
-    const { equipePedido } = Object.fromEntries(await request.formData());
-    
-    if (!equipePedido) {
-        return fail(400, { message: "Selecione uma equipe" });
-    }
+    requestEntry: async ({ request, params, locals }) => {
+        const { equipePedido } = Object.fromEntries(await request.formData());
 
-    try {
-        await prisma.PedidoCampeonato.create({
-            data: {
+        if (!equipePedido) {
+            // console.log("Selecione uma equipe");
+            return fail(500, {message: "Equipe não selecionada"});
+        }
+
+        const pedidoExists = await prisma.PedidoCampeonato.findFirst({
+            where: {
                 campeonatoId: Number(params.tournamentId),
                 equipeId: Number(equipePedido)
             }
         });
-    } catch (error) {
-        return fail(500, { message: "Não foi possível pedir a participação" });
+
+        if (pedidoExists) {
+            // console.log("Esta equipe ja foi pedida");
+            return fail(500, {message: "Esta equipe ja foi pedida"});
+        }
+
+        const equipeCampeonato = await prisma.EquipeDoCampeonato.findFirst({
+            where: {
+                id: Number(params.tournamentId),
+                equipeId: Number(equipePedido)
+            }
+        })
+
+        if (equipeCampeonato){
+            return fail(500, {message: "Esta equipe ja está no campeonato"});
+        }
+
+        try {
+            await prisma.PedidoCampeonato.create({
+                data: {
+                    campeonatoId: Number(params.tournamentId),
+                    equipeId: Number(equipePedido)
+                }
+            });
+            // console.log("Equipe pedida com sucesso");
+        } catch (error) {
+            // console.log("Não foi possível pedir a participação");
+            return fail(500, {message: "Não foi possivel pedir a participação"});
+        }
     }
-}
 };
