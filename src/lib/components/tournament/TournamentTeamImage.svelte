377ed93b3dc0242fdec5a564672591b230723cc0
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-11-20 21:37:02 -0300

    🎨 team image to component

---
+++ src/lib/components/tournament/TournamentTeamImage.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let foto
+    export let nome
+</script>
+
+<div class="image-wrapper">
+    <img src={foto} class="equipe-img" alt="Foto da equipe {nome}" title={nome} />
+</div>
+
+<style>
+    .image-wrapper {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        overflow: hidden;
+        border: #2e3856 solid 2px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .equipe-img {
+        max-width: auto;
+        max-height: 100%;
+        object-fit: cover;
+    }
+</style>
