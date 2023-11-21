6e58f5de9b04f43227f85e864714c884bb4c5a3f
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-11-20 21:40:44 -0300

    ✨ logic to fill cards

---
+++ src/routes/home/+page.server.js
@@ -0,0 +1,30 @@
+import { prisma } from '$lib/server/prisma';
+
+
+/** @type {import('./$types').PageLoad} */
+export async function load() {
+    const campeonatos = await prisma.Campeonato.findMany(
+        {
+            orderBy: {
+                dataCriacao: 'desc'
+            },
+            take: 3
+        }
+    );
+
+    const equipeDoCampeonato = await prisma.EquipeDoCampeonato.findMany(
+        {
+            where: {
+                campeonatoId: {
+                    in: campeonatos.map(campeonato => campeonato.id)
+                }
+            }
+        }
+    )
+    
+    console.log(equipeDoCampeonato);
+    
+    return {
+        campeonatos
+    }
+}
