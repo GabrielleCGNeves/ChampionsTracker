29c465db37334c31a182d48a14e5efe4b8642e62
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-11-27 20:02:48 -0300

    📝 added docstrings

--- src/lib/util/helpers.js
+++ src/lib/util/helpers.js
@@ -1,3 +1,8 @@
+/**
+ * Formats a date string into a specific format.
+ * @param {string} dataOriginal - The original date string to be formatted.
+ * @returns {string} The formatted date string.
+ */
 const dateFormat = (dataOriginal) => {
     let regex = /(\w{3}) (\w{3}) (\d{2}) (\d{4})/;
     let match = regex.exec(dataOriginal);
@@ -25,10 +30,21 @@ const dateFormat = (dataOriginal) => {
         return "Erro ao Formatar a Data";
     }
 };
+/**
+ * Calculates the sum of the number of players in each team.
+ * @param {Array} equipes - The array of teams.
+ * @returns {number} The sum of the number of players.
+ */
 const getSumOfJogadores = (equipes) => {
     return equipes.reduce((sum, equipe) => sum + equipe.numeroJogadores, 0);
 };
 
+/**
+ * Capitalizes the first letter of a string.
+ * 
+ * @param {string} string - The input string.
+ * @returns {string} The input string with the first letter capitalized.
+ */
 const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 };
